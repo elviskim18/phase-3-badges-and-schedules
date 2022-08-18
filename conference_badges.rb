cb843bee817140a0b8076c49975bee906f61068f
Kimani <elvis.kimani@student.moringaschool.com>
2022-08-18 10:02:03 +0300

    commit tree 6c92c865dcaf5d356b5648bcb3b673f4c56c33fb

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+    list = []
+    names.each do |name|
+        list << ("Hello, my name is #{name}.")
+    end
+    return list
+end
+
+def assign_rooms (speakers)
+    list= []
+    speakers.each_with_index {|speaker ,index|list << ("Hello, #{speaker}! You'll be assigned to room #{index+1}!")}
+    return list
+end 
+
+def printer(attendees)
+    batch_badge_creator(attendees).each {|names| puts names}
+    assign_rooms(attendees).each {|names| puts names}
+end
